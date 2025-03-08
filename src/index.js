8e9969825a55c983e8f63bd44919ea092b0f6c59
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-03-08 14:52:09 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import './index.css';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const element = document.getElementById('root');
+const root = createRoot(element);
+
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
